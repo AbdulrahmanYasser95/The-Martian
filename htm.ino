1551a614395d42d07a47e64c927b7ce6d71481fd
AbdulrahmanYasser95 <38386819+AbdulrahmanYasser95@users.noreply.github.com>
2018-10-20 11:42:12 +0200

    Add files via upload

---
+++ htm.ino
@@ -0,0 +1,29 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  Serial.print("Soil moisture level =");
+  Serial.println(analogRead(A0));
+  if (analogRead(A0)<200){
+    Serial.println("Irrigation: On");
+  }
+  else {
+    Serial.println("Irrigation: Off");
+    }
+  Serial.println("");
+  delay(1000);
+}
+
